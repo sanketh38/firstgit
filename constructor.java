cfd2f90fefaaa8f5984bf4680bc49aecf2049b62
Sanketh <sankethlgowda@gmail.com>
2022-03-05 19:59:53 +0530

    this is the newest commit that contains contructor illustrator java class

---
+++ constructor.java
@@ -0,0 +1,19 @@
+package __pycache__;
+
+public class constructor {
+    static class integers{
+        int x;
+        int y;
+    
+     integers(){
+        x =10;
+        y= 11;
+    }
+    //the above integers() code is a constructor.
+}
+    public static void main(String[] args) {
+        integers obj1 = new integers();
+        System.out.println("The values of the variable are "+obj1.x+" and "+obj1.y);
+    }
+}
+
