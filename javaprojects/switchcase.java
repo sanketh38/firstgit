e404cda448bcc18df73be19600b0758c1cf43640
Sanketh <sankethlgowda@gmail.com>
2022-03-06 17:49:53 +0530

    switch statement example added.

---
+++ javaprojects/switchcase.java
@@ -0,0 +1,19 @@
+package __pycache__;
+
+import java.util.Scanner;
+
+public class switchcase {
+    public static void main(String[] args) {
+    int x = 1;
+    switch(x) {
+        case 1:
+            System.out.println("ooo");
+            break;
+        case 2:
+           System.out.println("pookiee");
+        default:
+            break;
+    }
+    
+}
+}
