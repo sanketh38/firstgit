c56f872cbfe3bb2098c841aaf5d46718fb9af75e
Sanketh <sankethlgowda@gmail.com>
2022-03-06 19:11:18 +0530

    input example added

---
+++ javaprojects/scannerclass.java
@@ -0,0 +1,12 @@
+package __pycache__;
+
+import java.util.Scanner;
+
+public class scannerclass {
+public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+    int y = x.nextInt();
+    System.out.println(y);
+}    
+}
+
